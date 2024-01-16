e4ab8e3275cd0eb1fc76d4111220e98906114836
prafullKrRj <prafullkumar384@gmail.com>
2024-01-16 10:05:30 +0530

    Recipe Screen start

--- app/src/main/java/com/prafullkumar/recipeharbour/presentations/recipeScreen/components/RecipeDetails.kt
+++ app/src/main/java/com/prafullkumar/recipeharbour/presentations/recipeScreen/components/RecipeDetails.kt
@@ -1,21 +1,28 @@
 package com.prafullkumar.recipeharbour.presentations.recipeScreen.components
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment.Companion.Center
 import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.text.style.TextOverflow.Companion
-import androidx.compose.ui.text.style.TextOverflow.Companion.Clip
 import androidx.compose.ui.text.style.TextOverflow.Companion.Visible
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.prafullkumar.recipeharbour.model.singleRecipeDto.SingleRecipeDto
-import com.prafullkumar.recipeharbour.ui.theme.PoppinsMedium
 import com.prafullkumar.recipeharbour.ui.theme.PoppinsRegular
 import com.prafullkumar.recipeharbour.ui.theme.PoppinsSemiBold
 
@@ -31,41 +38,58 @@ fun RecipeDetails(modifier: Modifier = Modifier, recipeDto: SingleRecipeDto) {
                 text = it,
                 fontFamily = PoppinsSemiBold,
                 fontSize = 24.sp,
-                modifier = Modifier.fillMaxWidth().padding(top = 16.dp),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 16.dp),
                 overflow = Visible,
                 textAlign = TextAlign.Center
             )
         }
         if (recipeDto.recipe?.mealType != null && recipeDto.recipe?.totalTime != null) {
-            Text(
-                text = "${recipeDto.recipe?.mealType?.get(0)} | ${recipeDto.recipe?.totalTime} min",
-                fontFamily = PoppinsRegular,
-                fontSize = 16.sp,
-                fontWeight = FontWeight.W200,
-                modifier = Modifier.fillMaxWidth(),
-                overflow = Visible,
-                textAlign = TextAlign.Center
-            )
+            RecipeDetailsFieldText(text = "${recipeDto.recipe?.mealType?.get(0)} | ${recipeDto.recipe?.totalTime} min")
         } else if (recipeDto.recipe?.mealType != null) {
-            Text(
-                text = recipeDto.recipe?.mealType?.get(0) ?: "",
-                fontFamily = PoppinsMedium,
-                fontSize = 16.sp,
-                fontWeight = FontWeight.W200,
-                modifier = Modifier.fillMaxWidth(),
-                overflow = Visible,
-                textAlign = TextAlign.Center
-            )
+            RecipeDetailsFieldText(text = recipeDto.recipe?.mealType?.get(0) ?: "")
         } else if (recipeDto.recipe?.totalTime != null) {
-            Text(
-                text = "${recipeDto.recipe?.totalTime} min",
-                fontFamily = PoppinsMedium,
-                fontSize = 16.sp,
-                fontWeight = FontWeight.W200,
-                modifier = Modifier.fillMaxWidth(),
-                overflow = Visible,
-                textAlign = TextAlign.Center
-            )
+            RecipeDetailsFieldText(text = "${recipeDto.recipe?.totalTime} min")
+        }
+        HealthChartRow()
+    }
+}
+// procnt, fat, energy, chocdf, chocdf.net
+@Composable
+fun HealthChartRow(modifier: Modifier = Modifier) {
+    Row(
+        modifier = modifier.fillMaxWidth(),
+    ) {
+        repeat(4) {
+            HealthChartRowItem(modifier = Modifier.weight(.25f))
         }
     }
+}
+
+@Composable
+fun HealthChartRowItem(modifier: Modifier) {
+    Box(modifier = modifier
+        .padding(8.dp)
+        .size(80.dp)
+        .clip(CircleShape)
+        .shadow(1.dp, CircleShape),
+        contentAlignment = Center
+    ) {
+
+    }
+}
+@Composable
+fun RecipeDetailsFieldText(
+    text: String,
+) {
+    Text(
+        text = text,
+        fontFamily = PoppinsRegular,
+        fontSize = 16.sp,
+        fontWeight = FontWeight.W200,
+        modifier = Modifier.fillMaxWidth(),
+        overflow = Visible,
+        textAlign = TextAlign.Center
+    )
 }
