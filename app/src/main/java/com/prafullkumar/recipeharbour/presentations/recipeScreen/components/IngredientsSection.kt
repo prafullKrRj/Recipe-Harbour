0218bfa1b109f34028ecb07ebf7a094a34910aa3
prafullKrRj <prafullkumar384@gmail.com>
2024-01-17 19:46:25 +0530

    Recipe Screen mid

--- app/src/main/java/com/prafullkumar/recipeharbour/presentations/recipeScreen/components/IngredientsSection.kt
+++ app/src/main/java/com/prafullkumar/recipeharbour/presentations/recipeScreen/components/IngredientsSection.kt
@@ -1,34 +1,59 @@
 package com.prafullkumar.recipeharbour.presentations.recipeScreen.components
 
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.prafullkumar.recipeharbour.model.singleRecipeDto.Ingredients
 import com.prafullkumar.recipeharbour.model.singleRecipeDto.SingleRecipeDto
+import com.prafullkumar.recipeharbour.ui.theme.lightGrey
+import com.prafullkumar.recipeharbour.ui.theme.lightGrey100
 
 @Composable
 fun IngredientsSection(recipeDto: SingleRecipeDto, modifier: Modifier) {
     val ingredients = recipeDto.recipe?.ingredients ?: emptyList()
     Column {
-        ingredients.forEach {
-            IngredientItem(
-                modifier = modifier,
-                ingredient = it
-            )
-        }
+        IngredientTable(modifier = Modifier.padding(12.dp), ingredient = ingredients)
     }
 }
 
 @Composable
-fun IngredientItem(modifier: Modifier, ingredient: Ingredients) {
-    Column(modifier = modifier) {
-        Text(
-            text = ingredient.text ?: "",
-            fontSize = 16.sp,
-            fontWeight = FontWeight.Medium
-        )
+fun IngredientTable(modifier: Modifier, ingredient: List<Ingredients>) {
+    Column(
+        modifier = modifier
+            .fillMaxWidth()
+            .clip(RoundedCornerShape(8.dp))
+            .border(1.dp, lightGrey, RoundedCornerShape(8.dp))
+    ) {
+        Row(Modifier.fillMaxWidth()) {
+            Box(modifier = Modifier
+                .weight(.3f)
+                .border(1.dp, lightGrey, RoundedCornerShape(topStart = 8.dp)).padding(6.dp), contentAlignment = androidx.compose.ui.Alignment.Center) {
+                Text(
+                    text = "Ingredient",
+                    fontSize = 16.sp,
+                    fontWeight = FontWeight.Bold
+                )
+            }
+            Box(modifier = Modifier
+                .weight(.3f)
+                .border(1.dp, lightGrey, RoundedCornerShape(topEnd = 8.dp)).padding(6.dp), contentAlignment = androidx.compose.ui.Alignment.Center) {
+                Text(
+                    text = "Quantity",
+                    fontSize = 16.sp,
+                    fontWeight = FontWeight.Bold
+                )
+            }
+        }
     }
 }
